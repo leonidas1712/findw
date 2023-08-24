af3bd01ddb6aed760d79d8127913bd0efecc1066
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-08-25 07:13:30 +0800

    Setup reqwest and tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://www.google.com/";
+
+    let resp = reqwest::get(url)
+        .await?
+        .text()
+        .await;
+
+    match resp {
+        Ok(res) => {
+            println!("Response received");
+            println!("-----------------");
+            println!("{res}");
+        },
+        Err(err) => {
+            println!("Error requesting:{}", err);
+        }
+    }
+    Ok(())
 }
