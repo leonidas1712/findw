95580332cc7bad1bdc72c7d6b912a90f81f329c2
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-08-25 08:36:14 +0800

    Add concurr example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+
+type AsyncResult =  Result<(), Box<dyn std::error::Error>>;
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> AsyncResult {
+    concurrent().await;
+
+    Ok(())
+}
+
+async fn test(n:usize) {
+    println!("Test: {}", n);
+}
+
+async fn concurrent() -> AsyncResult {
+    let t1 = tokio::spawn({
+        test(10)
+    });
+
+    let t2 = tokio::spawn({
+        test(20)
+    });
+
+    let t3 = tokio::spawn({
+        test(30)
+    });
+
+    tokio::join!(t1, t2, t3);
+
+    Ok(())
+}
+
+async fn req() -> AsyncResult {
     let url = "https://www.google.com/";
 
     let resp = reqwest::get(url)
@@ -17,5 +48,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("Error requesting:{}", err);
         }
     }
+
     Ok(())
 }
