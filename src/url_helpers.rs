72c9605d735580055dd254296d7fe6be7bd96fb2
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-09-22 14:02:27 +0800

    Add get_info method

--- src/url_helpers.rs
+++ src/url_helpers.rs
@@ -24,9 +24,9 @@ impl Clone for ParsedUrl {
 
 /// Return result from get_info
 // TODO: change to use &str where possible
-struct InfoResult {
-    child_hrefs: Vec<String>,
-    page_title:Option<String>
+pub struct InfoResult {
+    pub child_hrefs: Vec<String>,
+    pub page_title:Option<String>
 }
 
 impl ParsedUrl {
@@ -36,15 +36,52 @@ impl ParsedUrl {
 
     // request full URL -> get child hrefs + document title
     pub async fn get_info(&self)->Result<InfoResult>{
+        // TODO: Can't do const with runtime type - use thread_local?
+        let title_selector = scraper::Selector::parse("title").unwrap();
+        // classes are to filter on wikipedia useless links
+        let link_selector = scraper::Selector::parse("a:not(.interlanguage-link-target, .mw-jump-link)").unwrap();
+
         let url = self.get_full_url();
         let html = reqwest::get(url)
             .await?
             .text()
             .await?;
 
+        let document = scraper::Html::parse_document(&html);
+    
+        // get hrefs raw
+        let links = document.select(&link_selector);
+        let links:Vec<_> = links.collect();
+
+        let links:Vec<String> = links.into_iter().filter_map(|elem| {
+            let val = elem.value();
+            let href = val.attr("href").map(|val| val.to_string());
+            return href;
+        }).collect();
+
+        println!("LINKS: {:?}", links);
+
+        // into_iter because a selector can technically match many elems, but title tag we only look at first
+        let title_select = document.select(&title_selector).into_iter().next(); 
+
+        // if no title found, placeholder value
+        let title = match title_select {
+            Some(elem) => elem.inner_html(),
+            None => { 
+                // println!("EMPTY");
+                String::from("(Empty title)") 
+            }
+        };
+
+        // title found but empty - None option
+        let opt_title = if title.len() == 0 {
+            None
+        } else {
+            Some(title)
+        };
 
         // ch
-        Ok(InfoResult { child_hrefs: vec![], page_title: Some("Title".to_string()) })
+        Ok(InfoResult { child_hrefs: links, page_title: opt_title })
 
     }
 }
