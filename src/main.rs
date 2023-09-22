b459f81e766c76240488e445e2fe047ad39c5158
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-09-22 13:12:39 +0800

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 use findw::search2::search2;
-use findw::url_helpers::debug_url;
+
+const URL:&'static str = "https://blog.janestreet.com/what-the-interns-have-wrought-2023/";
+const LOCAL_URL:&'static str = "http://localhost:8000/index.html";
+const BAD_URL:&'static str = "badurl";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("MAIN");
-    let URL = "https://blog.janestreet.com/what-the-interns-have-wrought-2023/";
-    let LOCAL_URL = "http://localhost:8000/index.html";
-    let BAD_URL = "badurl";
-
-    debug_url(LOCAL_URL);
-
-    search2(BAD_URL,"title",1).await?;
-
-    Ok(())
+    search2(BAD_URL,"title",1).await
 }
 
