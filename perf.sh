3fe466a87d8e5b9246b9f7f8ed0081cb6d884078
Leonidas Raghav <leonidasr2000@gmail.com>
2023-10-06 01:16:50 +0800

    Modify perf.sh

--- perf.sh
+++ perf.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 # generate perf for the .in
-if [ ! "$#" -eq 1 ]
+if [ ! "$#" -eq 2 ]
 then
-  echo "Expecting 1 argument (<test_case_name>), got $#"
+  echo "Expecting 2 arguments (<test_case_name> <runs>), got $#"
   exit 1
 fi
 
@@ -14,19 +13,20 @@ cargo b --release
 # TODO: make this work for all
 arr=(input/*.in)
 
-RUNS=3
+RUNS=$2
 
 if [[ "$1" == "all" ]]; then
   for file_name in ${arr[*]}; do
     # 0,1,jane...
     test_case_name=$(basename $file_name .in)
-    perf_file="perf/$test_case_name.perf"
+    perf_file="./perf/$test_case_name.perf"
     echo "Generating perf: $perf_file"
-   (hyperfine "xargs ./target/release/findw < input/$test_case_name.in" -r 3) > $perf_file
+   (hyperfine "xargs ./target/release/findw < input/$test_case_name.in" -r $RUNS) > $perf_file
     printf ""
   done
 else
-  echo "Generating perf/$1.perf..."
-#   xargs ./target/release/findw < input/$1.in > output/$1.out
+  perf_file="./perf/$1.perf"
+  echo "Generating $perf_file.."
+  (hyperfine "xargs ./target/release/findw < input/$1.in" -r $RUNS) > $perf_file
 fi
 
