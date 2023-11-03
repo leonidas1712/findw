3db5c97f836414da7c469f16a10a9bdddffa38bd
Leonidas Raghav <leonidasr2000@gmail.com>
2023-11-03 12:34:54 +0800

    Add analysis comments

--- jane.py
+++ jane.py
@@ -1,5 +1,8 @@
+# Analysis to check correctness of output for jane.in i.e
+    # https://blog.janestreet.com/what-the-interns-have-wrought-2023/ Jane 1
+
 # links from DOM API on page for jane.in, de-duped and filtered non-empty
-jane = set([
+jane_list = [
     "https://www.janestreet.com/ad-cookie-policy",
     "https://blog.janestreet.com/",
     "https://blog.janestreet.com/what-the-interns-have-wrought-2023/#menu",
@@ -63,7 +66,14 @@ jane = set([
     "https://www.finra.org/",
     "https://www.janestreet.com/privacy-policy/",
     "https://www.janestreet.com/ad-cookie-policy/"
-])
+]
+
+print(len(jane_list))
+jane_list = list(map(lambda x: x[:-1] if x[-1] == '/' else x, jane_list))
+print(len(jane_list))
+
+jane = set(jane_list)
+print(len(jane))
 
 with open('output/jane.out') as jane_out:
     text = jane_out.read()
@@ -77,10 +87,42 @@ with open('output/jane.out') as jane_out:
             child_set.add(child)
         
     print("jane.out:", len(child_set))
+    for x in sorted(child_set):
+        print(x)
+    print("-----")
     print("DOM API:", len(jane))
+    for y in sorted(jane):
+        print(y)
     
-    # what is in DOM API scrape but not in findw output?
-    st = jane.difference(child_set)
+    print("Difference: what is in DOM API scrape but not in findw output?")
+    print("-----")
+    st = jane.difference(sorted(child_set))
     
-    for link in st:
+    for link in sorted(st):
+        if (link+'/') in child_set:
+            continue
         print(link)
+        
+        
+# Diff output:
+
+# https://blog.janestreet.com/what-the-interns-have-wrought-2023/# - this is a cycle 
+
+## These two are relative links - search fn constructs as "https://blog.janestreet.com/" + "#menu" which is usually correct
+# for other rel links but for these 2 happen to be wrong
+    # Example: href='/tag/async' corresponds to 'https://blog.janestreet.com/tag/async'
+# https://blog.janestreet.com/what-the-interns-have-wrought-2023/#menu
+# https://blog.janestreet.com/what-the-interns-have-wrought-2023/#search
+
+# --- these don't have Jane in title ----
+# http://www.linkedin.com/shareArticle?mini=true&url=https://blog.janestreet.com/what-the-interns-have-wrought-2023/&title=Jane%20Street%20Tech%20Blog%20-%20What%20the%20interns%20have%20wrought,%202023%20edition&summary=We%E2%80%99re%20once%20again%20at%20the%20end%20of%20our%20internship%20season,%20and%20it%E2%80%99s%20my%20task%20to%20provide%20a%20few%20highlights%20of%20what%20the%20dev%20interns%20accomplished%20while%20they%20were%20here.%20The%20program%20was%20big!%20We%20had%20152%20software%20engineering%20interns,%20drawn%20from%2058%20schools%20across%2019%20different%20countries.%20And%20that%E2%80%99s%20not...&source=https://blog.janestreet.com/what-the-interns-have-wrought-2023
+# https://en.wikipedia.org/wiki/Linear_temporal_logic
+# https://github.com/inhabitedtype/angstrom
+# https://github.com/openai/tiktoken
+# https://signalsandthreads.com
+# https://signalsandthreads.com/a-poets-guide-to-product-management
+# https://signalsandthreads.com/state-machine-replication-and-why-you-should-care
+# https://twitter.com/intent/tweet?source=https://blog.janestreet.com/what-the-interns-have-wrought-2023/&text=Jane%20Street%20Tech%20Blog%20-%20What%20the%20interns%20have%20wrought,%202023%20edition%20https://blog.janestreet.com/what-the-interns-have-wrought-2023
+# https://www.facebook.com/sharer.php?u=https://blog.janestreet.com/what-the-interns-have-wrought-2023
+# https://www.finra.org
+# https://www.youtube.com/watch?v=Rt3XyeFHvt4
